18f0332bfa3cce010f65fa644d9c27500582bcd3
Valera Demin <wahrwolffe@gmail.com>
2024-04-06 00:13:30 +0300

    Module upd

--- modules/admin.py
+++ modules/admin.py
@@ -4,6 +4,6 @@ from modules.models import Module
 
 @admin.register(Module)
 class ModuleAdmin(admin.ModelAdmin):
-    list_display = ('serial_number', 'name', 'description', 'image', 'url_video', 'last_update', 'owner')
+    list_display = ('serial_number', 'name', 'description', 'url_video', 'last_update', 'owner', 'is_published')
     search_fields = ('serial_number', 'name', 'description', 'image', 'url_video', 'last_update', 'owner')
     list_filter = ('serial_number', 'name', 'description', 'image', 'url_video', 'last_update', 'owner')
--- modules/models.py
+++ modules/models.py
@@ -18,6 +18,7 @@ class Module(models.Model):
     url_video = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
     last_update = models.DateField(auto_now=True, verbose_name='последнее обновление')
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='владелец')
+    is_published = models.BooleanField(default=True, verbose_name='опубликован')
 
     def save(self, *args, **kwargs):
         """ Сохранение порядкового номера """
@@ -28,3 +29,11 @@ class Module(models.Model):
             else:
                 self.serial_number = 1
         super(Module, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'модуль'
+        verbose_name_plural = 'модули'
+        ordering = ('serial_number',)
